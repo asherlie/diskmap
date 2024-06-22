7b215d8a7158d1f8c1ad82011760890a7f7f1d1a
asherlie <lieber.asher@gmail.com>
2024-06-21 22:46:48 -0400

    removed comments that are no longer relevant

--- dm.c
+++ dm.c
@@ -1,26 +1,4 @@
 /*
- * okay, this works
- * now think about a lock free persistant hash map
- * allocate n files, read phash code
- * possibly add to that existing project actually
- * the only missing piece is a mmap'd lock free threadsafety mechanism
- * can't remember why this is needed, but this missing piece will allow safety for specific files actually!
- * oh wait NVM!! i think this just makes it so that i can have multiple processes using the same phash!!!
- *
- * should it be possible to insert variable size values? strings for example?
- * yes. it should be. both k and v should optionally be variable length
- * this means each bucket should have the following structure:
- *  bucket:
- *      {INT_key,INT_val,KEYTYPE_key,VALTYPE_val}, // entry 0
- *      {INT_key,INT_val,KEYTYPE_key,VALTYPE_val}, // entry 1
- *      {INT_key,INT_val,KEYTYPE_key,VALTYPE_val}  // entry 2
- * implementation details:
- *  diskmap:
- *      int n_buckets;
- *      char* buckets[n_buckets] // file names
- *      int bucket_sizes[n_buckets]
- *      int bucket_locks[n_buckets]
- *
  *
  * upon insertion we hash the key, find the bucket file for that key
  * insert(key, val):
